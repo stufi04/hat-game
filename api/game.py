8a666c343f712f462e95c8f10f6b450027a92ad8
stufi04 <st.ivanov44@gmail.com>
2020-04-11 18:21:30 +0100

    enable user to write words

--- api/game.py
+++ api/game.py
@@ -3,6 +3,7 @@ MAX_PLAYERS = 8
 
 class Game:
     players = []
+    words = []
 
     def __init__(self, player, code):
         self.code = code
@@ -11,3 +12,6 @@ class Game:
     def add_player(self, player):
         if len(self.players) < MAX_PLAYERS:
             self.players.append(player)
+
+    def add_word(self, word):
+        self.players.append(word)
--- src/Words.js
+++ src/Words.js
@@ -5,10 +5,21 @@ class Words extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {words: [], name: this.props.match.params.player, code: this.props.match.params.code, host: this.props.match.params.host}
+        this.state = {words: ['', '', '', '', ''], name: this.props.match.params.player, code: this.props.match.params.code, host: this.props.match.params.host}
 
+        this.submitWords = this.submitWords.bind(this)
+        this.handleWordChange = this.handleWordChange.bind(this)
     }
 
+    handleWordChange(index, event) {
+        var words = this.state.words.slice();
+        words[index] = event.target.value;
+        this.setState({words: words})
+    }
+
+    submitWords() {
+
+    }
 
 
     render() {
@@ -18,28 +29,34 @@ class Words extends React.Component {
         let buttons
         if (this.props.match.params.host == "true") {
             buttons = <div className="box">
-                        <button>Submit</button>
+                        <button onClick={this.submitWords}>Submit</button>
                         <button style={{marginLeft: 50}} >Start</button>
                     </div>
         } else {
             buttons = <button>Submit</button>
         }
 
+
+        let inputs = this.state.words.map((word, index) => {
+            return (
+                <div>
+                <input type="text"
+                       value={word}
+                       key={index}
+                       style={{marginBottom: 20}}
+                       onChange={this.handleWordChange.bind(this,index)}
+                />
+                </div>
+            );
+        })
+
+
         return (
             <div className="App">
                 <header className="App-header">
                     <h1>{this.state.name}, add your words to the hat:</h1>
                     <br/>
-                    <input type='text'></input>
-                    <br/>
-                    <input type='text'></input>
-                    <br/>
-                    <input type='text'></input>
-                    <br/>
-                    <input type='text'></input>
-                    <br/>
-                    <input type='text'></input>
-                    <br/>
+                    {inputs}
                     {buttons}
                     <br/>
                     <h3>Game code: {this.state.code}</h3>
